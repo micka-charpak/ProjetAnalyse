4a89c2df8604d336d34965826519cb6fbebd4fcc
micka <micka.renaud-92@live.fr>
2019-12-16 00:02:00 +0100

    active learning algorithm (pool based) to debug. pb with data structure and indexes...

--- Database_Titanic/active_villebon.py
+++ Database_Titanic/active_villebon.py
@@ -2,6 +2,7 @@ import pandas as pd
 import random
 import partie_estelle as pe
 import numpy as np
+import pandas as pd
 # data
 
 # model
@@ -9,7 +10,6 @@ import numpy as np
 pool_size = 3
 
 def strategy(probas, pool_size):
-    random_state = check_random_state(0)
     selection = np.random.choice(probas.shape[0], pool_size, replace=False)
 
     #     print('uniques chosen:',np.unique(selection).shape[0],'<= should be equal to:',initial_labeled_samples)
@@ -23,23 +23,33 @@ if __name__ == '__main__':
     # print(pe.download()[1])
     # print(pe.download()[1][4])
     (X_train, y_train, X_left, y_left, X_test, y_test) = pe.split(pe.download()[0],pe.download()[1])
-    while len(X_train) - pe.final_train_size > pool_size :
-        print(pe.randomForest(X_train, Y_train, X_test, Y_test))
+    X_train= pd.DataFrame(X_train)
+    y_train = pd.DataFrame(y_train)
+    X_left = pd.DataFrame(X_left)
+    y_left = pd.DataFrame(y_left)
+    X_test = pd.DataFrame(X_test)
+    y_test = pd.DataFrame(y_test)
+
+
+    while pe.final_train_size - len(X_train) > pool_size :
         resultats = pe.randomForest(X_train, y_train, X_left, y_left)
-        probas = resultats[5]
+        probas = resultats[1]
+        print(1)
+
 
         # on choisit des echantillons à labelliser
-        chosen_pool = strategy(proba, pool_size)
-
+        chosen_pool = strategy(probas, pool_size)
         # labbeliser et ajouter les échantillons au modele
-        X_train.append(X_left[i] for i in chosen_pool)
-        Y_train.append(Y_train[i] for i in chosen_pool)
+        print(chosen_pool)
+        print(X_train)
         for i in chosen_pool:
-            np.delete(X_left, i, 0)
+            X_train.append([X_left.iloc[i]])
+            y_train.append([y_train.iloc[i]])
         for i in chosen_pool:
-            np.delete(Y_test, i, 0)
-    print(pe.randomForest(X_train, y_train, X_test, y_test))
-    print(pe.randomForest(pe.download()[0][:pe.final_train_size],pe.download()[1][:pe.final_train_size],X_test,y_test))
+            X_left.drop(i,0)
+            y_left.drop(i, 0)
+    print(pe.randomForest(X_train, y_train, X_test, y_test)[2])
+    print(pe.randomForest(pe.download()[0][:pe.final_train_size],pe.download()[1][:pe.final_train_size],X_test,y_test)[2])
 
 # sample
 # train
